4c779b12e34e999861019097ad074c9a668a04f6
FCO <fernandocorrea@gmail.com>
2013-10-20 02:46:56 -0200

    Adding Groups

--- Acumulator.js
+++ Acumulator.js
@@ -1,24 +1,69 @@
 function Acumulator(agregator_func, callback) {
-	this.agregator_func = this.agregator.the_last_one;
-	if(callback)
-		this.callback = callback;
-	if(agregator_func) {
-		if(agregator_func instanceof Function) {
-			this.agregator_func = agregator_func;
-		} else {
-			if(this.agregator[agregator_func])
-				this.agregator_func = this.agregator[agregator_func];
-			else
-				throw "Agregator function '" + agregator_func + "' does not exists."
+	this.group = {};
+	if(agregator_func instanceof Object) {
+		console.log("agregator_func is a Object");
+		this.is_group = true;
+		for(var key in agregator_func) {
+			if(agregator_func.hasOwnProperty(key))
+				this.group[key] = new Acumulator(agregator_func[key][0], agregator_func[key][1]);
 		}
+	} else if(callback instanceof Object && !callback instanceof Function) {
+		console.log("callback is a Object");
+		this.is_group = true;
+		for(var key in callback) {
+			if(callback.hasOwnProperty(key))
+				this.group[key] = new Acumulator(agregator_func, callback[key]);
+		}
+	} else {
+		this.agregator_func = this.agregator.the_last_one;
+		if(callback)
+			this.callback = callback;
+		if(agregator_func) {
+			if(agregator_func instanceof Function) {
+				this.agregator_func = agregator_func;
+			} else {
+				if(this.agregator[agregator_func])
+					this.agregator_func = this.agregator[agregator_func];
+				else
+					throw "Agregator function '" + agregator_func + "' does not exists."
+			}
+		}
+		this.data		= {val: null};
 	}
-	this.data		= {val: null};
 }
 
 Acumulator.prototype = {
-	waiting_time:		3000,
+	_waiting_time:		3000,
+	_delayable:		true,
+	get waiting_time() {
+		return this._waiting_time
+	},
+	get delayable() {
+		return this._delayable
+	},
+	set waiting_time(data) {
+		if(this.is_group) {
+			for(var key in this.group) {
+				if(this.group.hasOwnProperty(key))
+					this.group[key].waiting_time = data;
+			}
+		} else {
+			this._waiting_time = data;
+		}
+	},
+	set delayable(data) {
+		if(this.is_group) {
+			for(var key in this.group) {
+				if(this.group.hasOwnProperty(key))
+					this.group[key].delayable = data;
+			}
+		} else {
+			this._delayable = data;
+		}
+	},
 	data:			null,
-	delayable:		true,
+	is_group:		false,
+	group:			null,
 	callback:		function(data){
 		console.log(JSON.stringify(data));
 	},
@@ -47,15 +92,22 @@ Acumulator.prototype = {
 		},
 	},
 	push:			function(value) {
-		this.agregator_func.call(this.data, value, this.callback);
-		if(this.tid && this.delayable) clearTimeout(this.tid);
-		if(!this.tid || this.delayable) {
-			var _this = this;
-			this.tid = setTimeout(function(){
-				_this.tid = null;
-				_this.callback.call(_this, _this.data.val);
-				_this.data.val	= null;
-			}, this.waiting_time);
+		if(this.is_group) {
+			for(var key in this.group) {
+				if(this.group.hasOwnProperty(key))
+					this.group[key].push(value);
+			}
+		} else {
+			this.agregator_func.call(this.data, value, this.callback);
+			if(this.tid && this.delayable) clearTimeout(this.tid);
+			if(!this.tid || this.delayable) {
+				var _this = this;
+				this.tid = setTimeout(function(){
+					_this.tid = null;
+					_this.callback.call(_this, _this.data.val);
+					_this.data.val	= null;
+				}, this.waiting_time);
+			}
 		}
 	},
 };
